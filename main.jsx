2b0c92c0fee0e416fcbcba4bde31e3e1f6fe986f
Lepavian <lpvianna@gmail.com>
2024-06-22 18:44:25 -0300

    estruturação das <div> na tela de ConsultInvestiment

--- main.jsx
+++ main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import BtnDefault from './src/components/BtnDefault/BtnDefault'
 import BtnOutline from './src/components/BtnOutline/BtnOutline'
+import ConsultInvestment from './src/pages/ConsultInvestment/ConsultInvestment' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -9,5 +10,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <br/>
     <br />
     <BtnOutline contentText="Clique"/>
+    <ConsultInvestment/>
   </React.StrictMode>,
 )
--- src/pages/ConsultInvestment/ConsultInvestment.jsx
+++ src/pages/ConsultInvestment/ConsultInvestment.jsx
@@ -1,9 +1,35 @@
 import './ConsultInvestment.css';
-
+import React from 'react';
 export default function ConsultInvestment() {
 
   return (
-    <>
-    </>
+
+    <div class = "container">
+
+<div class = "information"></div>
+
+<div class="graph">
+<h1>GRÁFICO</h1>
+</div>
+
+<div class = "retangulos"> 
+
+<div class="info-box">Vencimento</div>
+<div class="info-box">Valor inicial investido</div>
+<div class="info-box">Aportes mensais</div>
+<div class="info-box">Valor total investido</div>
+<div class="info-box">Lucro bruto</div>
+
+</div>
+
+<button class="alterar-btn"> 
+    <h2>Alterar</h2> 
+</button> 
+
+
+
+
+</div>
+
   )
 }
