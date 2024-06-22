d193d818377746289ab99f61dbfecf2d5af91a88
mau_gois <gois.mauricio12@gmail.com>
2024-06-22 03:18:06 -0300

    feat: add tailwind, css dos btn's e input form

--- src/components/InputForm/InputForm.jsx
+++ src/components/InputForm/InputForm.jsx
@@ -4,6 +4,7 @@ export default function InputForm(props) {
 
   return (
     <>
+      <span></span>
       <input type={props.typeInput} placeholder={props.placeholderInput} className='input-form' />
     </>
   )
