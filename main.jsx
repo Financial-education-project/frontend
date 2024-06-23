9ebc465e1cb9ece30d065b3103862e5ee7ba6c93
mau_gois <gois.mauricio12@gmail.com>
2024-06-23 20:51:06 -0300

    feat: add router-dom

--- main.jsx
+++ main.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import  LoginRegister from "./src/pages/LoginRegister/LoginRegister.jsx";
+import FormInvestment from "./src/pages/FormInvestment/FormInvestment.jsx";
+import ConsultInvestment from "./src/pages/ConsultInvestment/ConsultInvestment.jsx";
 // 1. import `ChakraProvider` component
 import { ChakraProvider } from '@chakra-ui/react';
-import FormInvestment from './src/pages/FormInvestment/FormInvestment';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginRegister />,
+  },
+
+  {
+    path: "/invest",
+    element: <ChakraProvider>
+              <FormInvestment/>
+            </ChakraProvider>,
+  },
+
+  {
+    path: "/consult",
+    element: <ConsultInvestment />,
+  },
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* // 2. Wrap ChakraProvider at the root of your app */}
-    <ChakraProvider>
-      <FormInvestment/>
-    </ChakraProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
