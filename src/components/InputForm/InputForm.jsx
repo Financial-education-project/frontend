54e5f8235472aa2445ecc192c38d37e71fe09e68
mau_gois <gois.mauricio12@gmail.com>
2024-06-26 03:54:00 -0300

    feat: deixando a telas de login e registro responsiva e alterando estilo dos buttons e input

--- src/components/InputForm/InputForm.jsx
+++ src/components/InputForm/InputForm.jsx
@@ -5,8 +5,8 @@ export default function InputForm(props) {
   return (
     <>
       <div class="relative">
-          <input type={props.typeInput} id="floating_filled" className="input peer" placeholder=" " />
-          <label for="floating_filled" className="label">{props.PlaceHolder}</label>
+          <input type={props.typeInput} id={props.idInput} className="input peer" placeholder=" " />
+          <label for={props.idInput} className="label">{props.PlaceHolder}</label>
       </div>
     </>
   )
--- src/pages/LoginRegister/LoginRegister.jsx
+++ src/pages/LoginRegister/LoginRegister.jsx
@@ -7,52 +7,58 @@ export default function LoginRegister() {
 
   return (
     <>
+      {/* Login e register ALL */}
       <main className='container-login-register'>
-        <div className='first-card'>
-          <div className='first-column rounded-l-lg'>
-            <h1>Seja bem-vindo!</h1>
-            <p>Faça seu registro para se cadastrar</p>
-            <BtnOutline contentText="Cadastra-se" btnPaddingOutline="px-16"/>
+        {/* login e register inner */}
+        <div className="card-inner">
+          {/* Login */}
+          <div className='first-card'>
+            <div className='first-column card-first-column'>
+              <h1>Seja bem-vindo!</h1>
+              <p>Faça seu registro para se cadastrar</p>
+              <BtnOutline contentText="Cadastra-se" btnPaddingOutline="px-16"/>
+            </div>
+            
+            <div className='second-column card-second-column'>
+              <h1>Faça seu login.</h1>
+              <p>Coloque seus dados</p>
+              <form action="" method="post">
+                <div className='inputs-form'>
+                  <InputForm typeInput="text" idInput="userLogin" PlaceHolder="Usuário"/>
+                  <InputForm typeInput="password" idInput="passwordLogin" PlaceHolder="Senha"/>
+                </div>
+
+                <div className='btn-form'>
+                  <BtnDefault contentText="Entrar" btnPaddingDefault="px-24"/>
+                </div>
+              </form>
+            </div>
           </div>
           
-          <div className='second-column rounded-r-lg'>
-            <h1>Faça seu login.</h1>
-            <p>Coloque seus dados</p>
-            <form action="" method="post">
-              <div className='inputs-form'>
-                <InputForm typeInput="text" PlaceHolder="Usuário"/>
-                <InputForm typeInput="password" PlaceHolder="Senha"/>
-              </div>
+          {/* Register */}
+          <div className='second-card'>
+            <div className='second-column card-first-column'>
+              <h1>Seja bem-vindo!</h1>
+              <p>Faça seu registro para se cadastrar</p>
+              <form action="" method="post">
+                <div className='inputs-form'>
+                  <InputForm typeInput="text" idInput="nameRegister" PlaceHolder="Nome completo"/>
+                  <InputForm typeInput="text" idInput="userRegister" PlaceHolder="Usuário"/>
+                  <InputForm typeInput="email" idInput="emailRegister" PlaceHolder="E-mail"/>
+                  <InputForm typeInput="password" idInput="passwordRegister" PlaceHolder="Senha"/>
+                </div>
 
-              <div className='btn-form'>
-                <BtnDefault contentText="Entrar" btnPaddingDefault="px-24"/>
-              </div>
-            </form>
-          </div>
-        </div>
-        
-        <div className='second-card'>
-          <div className='second-column rounded-l-lg'>
-            <h1>Seja bem-vindo!</h1>
-            <p>Faça seu registro para se cadastrar</p>
-            <form action="" method="post">
-              <div className='inputs-form'>
-                <InputForm typeInput="text" PlaceHolder="Nome completo"/>
-                <InputForm typeInput="text" PlaceHolder="Usuário"/>
-                <InputForm typeInput="email" PlaceHolder="E-mail"/>
-                <InputForm typeInput="password" PlaceHolder="Senha"/>
-              </div>
+                <div className='btn-form'>
+                  <BtnDefault contentText="Cadastra-se" btnPaddingDefault="px-24"/>
+                </div>
+              </form>
+            </div>
 
-              <div className='btn-form'>
-                <BtnDefault contentText="Cadastra-se" btnPaddingDefault="px-24"/>
-              </div>
-            </form>
-          </div>
-
-          <div className='first-column rounded-r-lg'>
-            <h1>Já tem uma conta?</h1>
-            <p>Clique no link abaixo e faça seu login</p>
-            <BtnOutline contentText="Login" btnPaddingOutline="px-16"/>
+            <div className='first-column card-second-column'>
+              <h1>Já tem uma conta?</h1>
+              <p>Clique no link abaixo e faça seu login</p>
+              <BtnOutline contentText="Login" btnPaddingOutline="px-16"/>
+            </div>
           </div>
         </div>
       </main>
